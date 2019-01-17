d214eebcd1b78231f6651d3211bf61bd4e174016
bbom <23fnsk23@naver.com>
2019-01-17 19:49:28 +0900

    add 2751 solution, (merge and heap sort 구현)

--- algorithm_2019/algorithm_2019/2751.c
+++ algorithm_2019/algorithm_2019/2751.c
@@ -13,24 +13,54 @@ int N;
 int num[1000001];
 int sorted[1000001];
 
+/* merge sort */
 void Merge_sort(int left, int right);
 void merge(int left, int mid, int right);
+
+/* heap sort */
+void insert_heap(int n);
+int delete_heap(int n);
+
+void swap(int* a, int* b);
 
 int main(int argc, char * argv[])
 {
     scanf("%d", &N);
     
-    for(int i=0; i<N; i++)
+    /* merge sort
+     for(int i=0; i<N; i++){
+     scanf("%d", &num[i]);
+     }
+     Merge_sort(0, N-1);
+     for(int i=0; i<N; i++){
+     printf("%d\n", num[i]);
+     }
+     */
+    
+    /* heap sort */
+    for(int i=1; i<N+1; i++){
         scanf("%d", &num[i]);
+        insert_heap(i);
+    }
     
-    Merge_sort(0, N-1);
-    
+    int ans;
     for(int i=0; i<N; i++){
-        printf("%d\n", num[i]);
+        ans = delete_heap(N-i);
+        printf("%d\n", ans);
     }
     
     return 0;
 }
+
+/* swap 함수 */
+void swap(int* a, int* b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+
 /* conquer & merge */
 void merge(int left, int mid, int right)
 {
@@ -70,4 +100,42 @@ void Merge_sort(int left, int right)
         Merge_sort(mid+1, right);
         merge(left, mid, right);
     }
+}
+
+/* heap 삽입 연산 */
+void insert_heap(int n)
+{
+    int i = n;
+    while(1){
+        if(i == 1) break;
+        /* 부모랑 비교해서 부모보다 내가 더 작으면 swap */
+        else if(num[i/2] > num[i]) {
+            swap(&num[i/2], &num[i]);
+            i /= 2;
+        }
+        else break;
+    }
+}
+
+/* heap 삭제 연산 */
+int delete_heap(int n)
+{
+    int min_val = num[1];
+    num[1] = num[n];    //마지막을 제일 상위노드에 위치
+    int i = 1;
+    int * min_child;    //두 자식 중 더 작은 자식
+    
+    /* heap 재배열 */
+    while(1){
+        if(2*i > n) break;
+        if(2*i == n || (num[2*i] <= num[2*i+1])) min_child = &num[2*i];
+        else min_child = &num[2*i+1];
+        
+        if(num[i] > *min_child) {
+            swap(&num[i], min_child);
+            i = (int)(min_child - &num[0]); //현재 index값 계산
+        }
+        else break;
+    }
+    return min_val;
 }
